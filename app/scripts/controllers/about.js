b9d367975c6fae438cb7d2ac873a5c827b1c9ebd
Rodrigo Vallejo <rxvallejoc@gmail.com>
2014-11-22 19:29:05 -0500

    Starting angular ui for factronica.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name factronicaUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the factronicaUiApp
+ */
+angular.module('factronicaUiApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
